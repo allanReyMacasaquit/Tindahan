905eb82cb277c0fc62b1d1783eadc7d2534399f0
allan <allanrey.macasaquit@gmail.com>
2025-08-06 00:01:11 -0400

    3. Navbar Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-import { Button } from '@/components/ui/button';
-
 function HomePage() {
-	return (
-		<div>
-			HomePage
-			<div>
-				<Button variant='default' className='ml-1'>
-					Click me
-				</Button>
-			</div>
-		</div>
-	);
+	return <div>Home</div>;
 }
 export default HomePage;
